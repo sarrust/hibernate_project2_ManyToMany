f339f10004904e2b07ce5908146a15eb58b4d872
sarrust <sa.rustamoff@yandex.ru>
2023-02-08 20:29:50 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,38 @@
+package org.example;
+
+
+import org.example.model.Actor;
+import org.example.model.Movie;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Movie.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        try(sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            Actor actor = session.get(Actor.class, 1);
+            System.out.println(actor.getMovies());
+
+            Movie movieToRemove = actor.getMovies().get(0);
+
+            actor.getMovies().remove(0);
+            movieToRemove.getActors().remove(actor);
+
+
+
+            session.getTransaction().commit();
+        }
+    }
+}
